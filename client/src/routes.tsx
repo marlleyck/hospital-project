d1abbcf0cda9872bd93b4f0f3e857d0cf5a7a67f
marlleyck <marlleyck@hotmail.com>
2023-02-24 17:04:53 -0300

    feat: add login page

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
+import { LoginPage } from "./pages/Login";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
