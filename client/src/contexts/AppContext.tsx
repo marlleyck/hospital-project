734d34416a87309b2a0abac6da81b5c390dca882
marlleyck <marlleyck@hotmail.com>
2023-02-24 17:00:26 -0300

    feat: create AppContext type

--- client/src/contexts/AppContext.tsx
+++ client/src/contexts/AppContext.tsx
@@ -1,10 +1,12 @@
 import { createContext } from "react";
 
+import { AppContextType } from "../@types/AppContextType";
+
 type AppContextProviderProps = {
   children: JSX.Element;
 };
 
-export const AppContext = createContext({});
+export const AppContext = createContext({} as AppContextType);
 
 export const AppContextProvider = ({ children }: AppContextProviderProps) => {
   return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
