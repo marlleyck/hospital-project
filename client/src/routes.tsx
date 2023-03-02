501ad30022b03fd331cac91834607c67870f4c79
marlleyck <marlleyck@hotmail.com>
2023-03-01 21:18:36 -0300

    feat: add home page to private route

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./pages/Login";
 import { RegisterPage } from "./pages/Register";
 import { Home } from "./pages/Home";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 const AppRoutes = () => {
   return (
@@ -10,7 +11,14 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
