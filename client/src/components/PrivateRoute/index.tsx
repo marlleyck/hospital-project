a57180461ac941e106b88541a881b95774633488
marlleyck <marlleyck@hotmail.com>
2023-03-01 21:12:38 -0300

    feat: create component private route

---
+++ client/src/components/PrivateRoute/index.tsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+
+import { AuthContext } from "../../contexts/AuthContext";
+
+export const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+  const { authorized } = useContext(AuthContext);
+  return (
+    <>{authorized !== null && authorized ? children : <Navigate to="/" />}</>
+  );
+};
