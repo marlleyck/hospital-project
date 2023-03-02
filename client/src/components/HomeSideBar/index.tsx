f86dcb56f6bde152898cb2bb9e7a55c82d1e5157
marlleyck <marlleyck@hotmail.com>
2023-03-02 20:02:57 -0300

    feat: add paths in home page links

--- client/src/components/HomeSideBar/index.tsx
+++ client/src/components/HomeSideBar/index.tsx
@@ -1,12 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import { Container, Content, Title } from "./styles";
 
 export const HomeSideBar = () => {
+  const navigate = useNavigate();
   return (
     <Container>
       <Content>
-        <Title>Home</Title>
-        <Title>Adicionar Paciente</Title>
-        <Title>Pacientes</Title>
+        <Title onClick={() => navigate("/home")}>Home</Title>
+        <Title onClick={() => navigate("/addpatient")}>
+          Adicionar Paciente
+        </Title>
+        <Title onClick={() => navigate("/patients")}>Pacientes</Title>
       </Content>
     </Container>
   );
