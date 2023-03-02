09df395e784654dfaf2f23177e1c30d473df04ca
marlleyck <marlleyck@hotmail.com>
2023-03-01 21:16:30 -0300

    feat: create path to home

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { LoginPage } from "./pages/Login";
 import { RegisterPage } from "./pages/Register";
+import { Home } from "./pages/Home";
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
