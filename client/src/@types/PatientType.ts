01b96486825fe4880582582ac77c4f5c126dae43
marlleyck <marlleyck@hotmail.com>
2023-03-01 21:40:46 -0300

    chore: add patient type

---
+++ client/src/@types/PatientType.ts
@@ -0,0 +1,13 @@
+export type PatientType = {
+  id: string;
+  name: string;
+
+  weight: number;
+  height: number;
+  imc: number;
+
+  status_description: string;
+  blood_type: string;
+
+  doctor: string;
+};
