2fc322fda2344095b92cfdba852e162a7c15a514
marlleyck <marlleyck@hotmail.com>
2023-03-01 21:37:49 -0300

    feat: add user type

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
 
 import { AuthContextType } from "../@types/AuthContextType";
+import { UserType } from "../@types/UserType";
 
 type AuthProviderProps = {
   children: JSX.Element;
@@ -9,10 +10,10 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextType);
 
 export const AuthContextProvider = ({ children }: AuthProviderProps) => {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState<UserType>();
 
   const [token, setToken] = useState("");
-  const [authorized, setAuthorized] = useState<boolean | null>(null);
+  const [authorized, setAuthorized] = useState<boolean | null>(true);
 
   return (
     <AuthContext.Provider value={{ authorized }}>
