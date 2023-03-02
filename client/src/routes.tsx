b5fe364686e9c69fc14b8863b9109a7aa1644026
marlleyck <marlleyck@hotmail.com>
2023-03-01 21:03:48 -0300

    refactor: login page to default

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Home } from "./pages/Home";
 import { LoginPage } from "./pages/Login";
 import { RegisterPage } from "./pages/Register";
 
@@ -8,8 +7,7 @@ const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
